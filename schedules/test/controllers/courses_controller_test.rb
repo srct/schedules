baf8caf89d182c3f1f75310ff216ff3866c5119b
Zachary Wood <zac.wood9@gmail.com>
2018-04-08 14:18:38 -0400

    Fixed controller tests

--- schedules/test/controllers/courses_controller_test.rb
+++ schedules/test/controllers/courses_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class CoursesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get url_for :controller => 'courses', :action => 'index'
+    get url_for controller: 'courses', action: 'index'
     assert_response :success
   end
 
--- schedules/test/controllers/sections_controller_test.rb
+++ schedules/test/controllers/sections_controller_test.rb
@@ -1,15 +1,9 @@
 require 'test_helper'
 
 class SectionsControllerTest < ActionDispatch::IntegrationTest
-  # TODO: Fix SectionsController tests
-  # test "should get index" do
-  #   get url_for action: 'index', controller: 'sections'
-  #   assert_response :success
-  # end
-  #
-  # test "should get show" do
-  #   get url_for controller: 'sections', action: 'show', id: '1'
-  #   assert_response :success
-  # end
-
+  test "should get index" do
+    get url_for controller: 'sections', action: 'index', course_id: 1
+    # get 'api/'
+    assert_response :success
+  end
 end
