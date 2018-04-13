bfd24d738e652ab68d584f8be9a4f3eae6623676
Zachary Wood <zac.wood9@gmail.com>
2018-04-12 20:43:14 -0400

    Added tests for Search controller

--- schedules/app/controllers/search_controller.rb
+++ schedules/app/controllers/search_controller.rb
@@ -1,7 +1,11 @@
 class SearchController < ApplicationController
   def index
-    crn = params[:crn]
-    @sections = Section.find_by_crn(crn)
-    render json: @sections
+    if params.key?(:crn)
+      crn = params[:crn]
+      @sections = Section.find_by_crn(crn)
+      render json: @sections
+    else
+      render status: 404
+    end
   end
 end
--- schedules/test/controllers/search_controller_test.rb
+++ schedules/test/controllers/search_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class SearchControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get search_index_url
+  test "should get index and search by crn" do
+    get url_for controller: 'search', action: 'index', crn: 'MyString'
     assert_response :success
   end
 
+  test "should 404 without crn" do
+    get url_for controller: 'search', action: 'index'
+    assert_response :missing
+  end
 end
