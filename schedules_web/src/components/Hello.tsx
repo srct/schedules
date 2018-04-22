ff0fd7a40871528a3dae6dac8f28a5fc42a8bec4
Zachary Wood <zac.wood9@gmail.com>
2018-04-21 22:49:27 -0400

    Basic Typescript + React webpack config

---
+++ schedules_web/src/components/Hello.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export interface Props {
+    compilier: string;
+    framework: string;
+}
+
+export const Hello = (props: Props) => <h1>Hello from {props.compilier} and {props.framework}</h1>;
--- schedules_web/src/index.js
+++
@@ -1,7 +0,0 @@
-function component() {
-    const element = document.createElement('div');
-    element.innerHTML = "Hi";
-    return element;
-}
-
-document.body.appendChild(component());
