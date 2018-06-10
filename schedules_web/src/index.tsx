7d3267e4aaa52d8ff3ef98f57fe0b66d842226c3
David Haynes <dhaynes3@gmu.edu>
2018-06-10 15:33:37 -0400

    Add masonstrap
    - I probably went overkill on the loaders but I had to be certain that
    it'd work

--- schedules_web/src/index.tsx
+++ schedules_web/src/index.tsx
@@ -1,3 +1,5 @@
+import 'masonstrap/build/css/masonstrap.min.css';
+import 'masonstrap/build/js/masonstrap.min.js';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App';
--- schedules_web/webpack.config.js
+++ schedules_web/webpack.config.js
@@ -37,6 +37,43 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'source-map-loader'
             },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.woff(\?.+)?$/,
+                use: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.woff2(\?.+)?$/,
+                use: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?.+)?$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.eot(\?.+)?$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.svg(\?.+)?$/,
+                use: 'file-loader'
+            },
+
+
+            {
+                test: /\.(js)$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader"
+            },
         ],
     },
 
