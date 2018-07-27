6456a846924b174aa8f456ae6db3d19f1beb486c
Zachary Wood <zac.wood9@gmail.com>
2018-07-26 22:17:46 -0400

    i messed up

--- schedules_api/start.sh
+++ schedules_api/start.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# generate a secret key for rails to use
 export SECRET_KEY_BASE=$(rails secret)
-cp db/development.sqlite3 db/production.sqlite3
+
+# uncomment for faster docker builds during testing
+#cp db/development.sqlite3 db/production.sqlite3
+
+# load data from patriot web into database
+rails db:migrate
+rails db:seed
+
+# start the server
 rails s
