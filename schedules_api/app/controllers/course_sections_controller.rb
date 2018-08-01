cdadfe652bc86d980bc8f975b4b156edcecd8f16
Zachary Wood <zac.wood9@gmail.com>
2018-08-01 17:37:58 -0400

    Added search by CRN for course sections

--- schedules_api/app/controllers/course_sections_controller.rb
+++ schedules_api/app/controllers/course_sections_controller.rb
@@ -3,7 +3,11 @@
 class CourseSectionsController < ApplicationController
   # Render JSON of all Sections belonging to a given Course.
   def index
-    @sections = CourseSection.where(course_id: params[:course_id])
+    @sections = CourseSection.all
+
+    @sections = @sections.where(course_id: params[:course_id]) if params.key?(:course_id)
+    @sections = @sections.where(crn: params[:crn]) if params.key?(:crn)
+    
     render json: @sections
   end
 end
--- schedules_api/test/controllers/course_sections_controller_test.rb
+++ schedules_api/test/controllers/course_sections_controller_test.rb
@@ -10,4 +10,12 @@ class CourseSectionsControllerTest < ActionDispatch::IntegrationTest
 
     assert_equal num_sections, sections_returned.count
   end
+
+  test 'should filter by crn' do
+    get course_sections_url crn: course_sections(:cs112001).crn
+    assert_response :success
+
+    sections_returned = JSON.parse @response.body
+    assert_equal course_sections(:cs112001).name, sections_returned[0]["name"]
+  end
 end
