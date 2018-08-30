26009a8b2810d0c4b1078fe926126da474c5a84b
Zac Wood <zwood2@gmu.edu>
2018-08-30 16:31:56 -0400

    Changed deployment process to be much more robust
    - Added simple server to serve `dist/index.html` in prod
    - Split webpack config into common, dev, and prod files
    - Added an environment check in ApiService.ts to determine which endpoint to use

--- schedules_web/webpack.common.js
+++ schedules_web/webpack.common.js
@@ -11,9 +11,6 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 });
 
 module.exports = {
-    mode: 'development',
-    devtool: 'source-map',
-
     entry: './src/index.tsx',
 
     output: {
@@ -77,14 +74,4 @@ module.exports = {
     },
 
     plugins: [new webpack.HotModuleReplacementPlugin(), new CleanWebpackPlugin(['dist']), HtmlWebpackPluginConfig],
-
-    devServer: {
-        contentBase: path.resolve(__dirname, 'dist'),
-        compress: true,
-        host: '0.0.0.0',
-        port: 8080,
-        hot: true,
-        publicPath: '/',
-        historyApiFallback: true,
-    },
 };
