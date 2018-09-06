e3349b4abd46d8ded4674bae07aa938cce8d805a
Zac Wood <zwood2@gmu.edu>
2018-09-06 12:00:30 -0400

    Added Dockerfiles for use during development

--- init.sh
+++ init.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
+# Build the API image
 cd schedules_api/
-docker build . -t 'schedules_api'
+docker build . -t 'schedules_api' -f Dockerfile.dev
 
+# Build the Web image
 cd ../schedules_web
-docker build . -t 'schedules_web'
+docker build . -t 'schedules_web' -f Dockerfile.dev
 
+# Start and link the images together
 cd ..
 docker-compose up
