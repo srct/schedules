a9f9c64d8751fd23f0330e331d86f40a62295e0f
Zac Wood <zwood2@gmu.edu>
2018-09-19 21:19:02 -0400

    small fixes

--- schedules_api/app/controllers/search_controller.rb
+++ schedules_api/app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
   def index
     @courses = Course.where(subject: 'GEOL').select do |course|
-      course.course_sections.count > 0
+      course.course_sections.count.positive?
     end
-    
+
     @cart = cookies[:ids].split(',').map do |crn|
       CourseSection.find_by_crn crn
     end
