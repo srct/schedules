25bd1134875d09d19b10f7f07e2d95440fb1c4d4
Zac Wood <zwood2@gmu.edu>
2018-09-24 22:56:09 -0400

    Added instructor page and links on section cards to its instructor's page

--- schedules/config/routes.rb
+++ schedules/config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'search', to: 'search#index'
   get 'search/update', to: 'search#update'
 
+  resources :instructors, only: [:index, :show]
+
   scope :api do # Register /api routes
     resources :courses, only: [:index, :show]
     resources :course_sections, only: [:index]
