4d7d8564907184bc7a4fa5cae4d8023f9629c2e5
Zac Wood <zwood2@gmu.edu>
2019-03-16 15:30:10 -0400

    temparary fix for fall weirdness

--- schedules/db/seeds.rb
+++ schedules/db/seeds.rb
@@ -117,7 +117,7 @@ def main
                 [parser.parse_semesters.first]
               else
                 # expand to include however many semesters you want
-                parser.parse_semesters[0..6]
+                parser.parse_semesters[0..7]
               end
 
   puts "\tParsing subjects..."
